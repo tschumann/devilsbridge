8dc8456444f37caad91aef0d1e2cbc36dd9c7905
tschumann <schumann.tom@gmail.com>
2017-08-27 14:30:49 +1000

    Added tests for real.

---
+++ devilsbridge_tests/PlayerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace devilsbridge_tests
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "A null game was passed to the constructor.")]
+        public void testInstantiateNull()
+        {
+            devilsbridge.Player player = new devilsbridge.Player(null);
+        }
+    }
+}
---
+++ devilsbridge_tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("devilsbridge_tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("devilsbridge_tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("941ec134-24a3-4228-b496-bdac526b4956")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
