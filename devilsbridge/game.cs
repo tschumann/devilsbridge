cffd9675b1fd24aba1be2fbf459568627b2cb69d
tschumann <schumann.tom@gmail.com>
2017-10-14 17:32:35 +1000

    Added game attributes.

--- devilsbridge/game.cs
+++ devilsbridge/game.cs
@@ -1,13 +1,26 @@
-﻿
+﻿using System.Collections.Generic;
+
+using Card;
+
 namespace devilsbridge
 {
     public class Game : Card.IGame
     {
-        static void Main(string[] args)
+        private List<ICardPlayer> players;
+        private Deck deck;
+
+        public Game()
         {
+            this.players = new List<ICardPlayer>();
+            this.deck = new Deck(this, false);
         }
 
         public void deal()
+        {
+            this.deck.shuffle();
+        }
+
+        static void Main(string[] args)
         {
         }
     }
