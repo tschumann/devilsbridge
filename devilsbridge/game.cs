e183867fefc96c10755cb069e4f4da015c14579d
tschumann <schumann.tom@gmail.com>
2018-05-11 22:00:24 +1000

    Started adding play method to game.

--- devilsbridge/game.cs
+++ devilsbridge/game.cs
@@ -6,11 +6,13 @@ namespace devilsbridge
 {
     public class Game : Card.IGame
     {
-        private List<ICardPlayer> players;
+        private int round;
         private Deck deck;
+        private List<ICardPlayer> players;
 
         public Game(int playerCount)
         {
+            this.round = 1;
             this.deck = new Deck(this, false);
             this.players = new List<ICardPlayer>(playerCount);
 
@@ -23,6 +25,18 @@ namespace devilsbridge
         public void deal()
         {
             this.deck.shuffle();
+        }
+
+        public void play()
+        {
+            List<ICard> cards = new List<ICard>();
+
+            foreach(ICardPlayer player in this.players)
+            {
+                cards.Add(player.play());
+            }
+
+            round++;
         }
 
         public List<ICardPlayer> getPlayers()
